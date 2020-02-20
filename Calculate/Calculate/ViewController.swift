c1493108f5abc6d63286f4a5e2b43926793b5681
ildison <ildarusmanov92@gmail.com>
2020-02-20 18:32:16 +0300

    update calc

--- Calculate/Calculate/ViewController.swift
+++ Calculate/Calculate/ViewController.swift
@@ -7,53 +7,79 @@
 //
 
 import UIKit
+//
+//enum oper {
+//    case add
+//    case sub
+//    case mul
+//    case div
+//}
 
 class ViewController: UIViewController {
-
-//    extension UIButton {
-//        var cornerRadius: CGFloat {
-//            set { layer.cornerRadius = newValue  }
-//            get { return layer.cornerRadius }
-//        }
-//    }
-    @IBAction func Zero(_ sender: Any) {
-    }
-    @IBAction func One(_ sender: UIButton) {
-    }
-    @IBAction func Two(_ sender: UIButton) {
-    }
-    @IBAction func Three(_ sender: UIButton) {
-    }
     
-    @IBAction func Four(_ sender: UIButton) {
-    }
+    var stack = [String]()
     
-    @IBAction func Five(_ sender: UIButton) {
-    }
+    @IBOutlet weak var elementsLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func Six(_ sender: UIButton) {
-    }
-    
-    @IBAction func Seven(_ sender: UIButton) {
-    }
-    @IBAction func Eight(_ sender: UIButton) {
-    }
-    @IBAction func Nine(_ sender: Any) {
-
-    }
-    
-    @IBAction func Add(_ sender: UIButton) {
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func join(_ appended: String, _ label: UILabel) {
+        if label.text == "0" {
+            label.text = appended
+        }
+        else if label.text != "-0" {
+            label.text?.append(appended)
+        }
+        else {
+            label.text = "-" + appended
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numbers(_ sender: UIButton) {
+        join(String(sender.tag), resultLabel)
     }
 
+    @IBAction func dot() {
+        if resultLabel.text?.contains(".") == false {
+            resultLabel.text?.append(".")
+        }
+    }
+    
+    @IBAction func opers(_ sender: UIButton) {
+        let oper = ["+", "-", "x", "÷"]
+        guard let number = resultLabel.text else { return }
+        if number != "0" && number != "-0" {
+            join(number, elementsLabel)
+            stack.append(number)
+            resultLabel.text = "0"
+        }
+        else if elementsLabel.text!.count > 2 {
+            elementsLabel.text?.removeLast(3)
+            stack.removeLast()
+        }
+        join(" \(oper[sender.tag]) ", elementsLabel)
+        stack.append(oper[sender.tag])
+    }
 
+    @IBAction func plusMinus() {
+        guard let num = resultLabel.text else { return }
+        if num.first == "-" {
+            resultLabel.text?.removeFirst()
+        }
+        else {
+            resultLabel.text = "-" + num
+        }
+    }
+    
+    @IBAction func percent() {
+    }
+    @IBAction func result() {
+
+    }
+    
+    
+    @IBAction func clearResult(_ sender: UIButton) {
+        resultLabel.text = "0"
+    }
+    
 }
 
