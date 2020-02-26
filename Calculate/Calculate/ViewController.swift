3daad8f979786135d9c279ab368ac14591eded30
ildison <ildarusmanov92@gmail.com>
2020-02-26 18:16:15 +0300

    add class Calculate

--- Calculate/Calculate/ViewController.swift
+++ Calculate/Calculate/ViewController.swift
@@ -8,11 +8,65 @@
 
 import UIKit
 
+class Calculate {
+    var lastOper: String = ""
+    var stack = [String]()
+
+    private func isNumber(_ checked: String) -> Bool {
+        guard let firstCharacter = checked.first else {
+            return false
+        }
+        if firstCharacter >= "0" && firstCharacter <= "9" {
+            return true
+        }
+        return false
+    }
+    
+    private func  executeOper(oper: String, num1: Double, num2: Double) -> String {
+        switch oper {
+        case "+":
+            return "\(num1 + num2)"
+        case "-":
+            return "\(num1 - num2)"
+        case "x":
+            return String(num1 * num2)
+        case "÷":
+            return String(num1 / num2)
+        default:
+            return ""
+        }
+    }
+
+    func addInStack(_ elem: String) {
+        if isNumber(elem) && !stack.isEmpty && (stack.last == "x" || stack.last == "÷") {
+            let oper = stack.removeLast()
+            guard let num1 = Double(stack.removeLast()) else { return }
+            guard let num2 = Double(elem) else { return }
+            stack.append(executeOper(oper: oper, num1: num1, num2: num2))
+        }
+        else {
+            stack.append(elem)
+        }
+    }
+    
+    func result() -> String {
+        var countElems = stack.count
+
+        print(stack)
+        while countElems > 1 {
+            guard let num1 = Double(stack.removeFirst()) else { return ""}
+            let oper = stack.removeFirst()
+            guard let num2 = Double(stack.removeFirst()) else { return ""}
+            stack.insert(executeOper(oper: oper, num1: num1, num2: num2), at: 0)
+            countElems -= 2
+        }
+        return stack.removeFirst()
+    }
+}
 
 class ViewController: UIViewController {
-    
-    var stack = [String]()
-    var lastOper: String = ""
+
+    var calc = Calculate()
     var ac = true
     
     @IBOutlet weak var elementsLabel: UILabel!
@@ -37,9 +91,9 @@ class ViewController: UIViewController {
             ac = false
             clear.setTitle("C", for: .normal)
         }
-        if !lastOper.isEmpty {
-            stack.append(lastOper)
-            lastOper = ""
+        if !calc.lastOper.isEmpty {
+            calc.addInStack(calc.lastOper)
+            calc.lastOper = ""
         }
     }
 
@@ -61,25 +115,18 @@ class ViewController: UIViewController {
             number.removeLast()
             resultLabel.text?.removeLast()
         }
-//        if number != "0" && number != "-0" {
-        if lastOper.isEmpty {
+        if calc.lastOper.isEmpty {
             joinInLabel(number, elementsLabel)
-//            if !lastOper.isEmpty {
-//                stack.append(oper[sender.tag])
-//            }
-            stack.append(number)
+            calc.addInStack(number)
             resultLabel.text = "0"
         }
         else {
             elementsLabel.text?.removeLast(3)
-//            stack.removeLast()
         }
-        if !stack.isEmpty {
+        if !calc.stack.isEmpty {
             joinInLabel(" \(oper[sender.tag]) ", elementsLabel)
-            lastOper = oper[sender.tag]
-//            stack.append(oper[sender.tag])
+            calc.lastOper = oper[sender.tag]
         }
-        print(stack)
     }
 
     @IBAction func plusMinus() {
@@ -95,6 +142,26 @@ class ViewController: UIViewController {
     @IBAction func percent() {
     }
     @IBAction func result() {
+        if !calc.stack.isEmpty {
+            guard var number = resultLabel.text else { return }
+            if number.last == "." {
+                number.removeLast()
+                resultLabel.text?.removeLast()
+            }
+            if calc.lastOper.isEmpty {
+                joinInLabel(number, elementsLabel)
+                calc.addInStack(number)
+                resultLabel.text = "0"
+            }
+            else {
+                elementsLabel.text?.removeLast(3)
+            }
+            joinInLabel(" = ", elementsLabel)
+            resultLabel.text = calc.result()
+            calc.lastOper = ""
+            ac = true
+            clear.setTitle("AC", for: .normal)
+        }
     }
     
     
@@ -107,10 +174,9 @@ class ViewController: UIViewController {
         else {
             elementsLabel.text = "0"
             resultLabel.text = "0"
-            stack.removeAll()
-            lastOper = ""
+            calc.stack.removeAll()
+            calc.lastOper = ""
         }
     }
-    
 }
 
