b0c42c4d7688ec2654a0c49da17310ce64dc7e88
ildison <ildarusmanov92@gmail.com>
2020-02-27 17:00:56 +0300

    update result

--- Calculate/Calculate/ViewController.swift
+++ Calculate/Calculate/ViewController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 
+enum error: Error {
+    case Error
+}
+
 class Calculate {
     var lastOper: String = ""
     var stack = [String]()
 
-    private func isNumber(_ checked: String) -> Bool {
+    func isNumber(_ checked: String) -> Bool {
         guard let firstCharacter = checked.first else {
             return false
         }
-        if firstCharacter >= "0" && firstCharacter <= "9" {
-            return true
-        }
-        return false
+        return firstCharacter >= "0" && firstCharacter <= "9"
     }
     
-    private func  executeOper(oper: String, num1: Double, num2: Double) -> String {
+    private func  executeOper(oper: String, num1: Double, num2: Double) throws -> String {
         switch oper {
         case "+":
             return "\(num1 + num2)"
@@ -31,22 +32,37 @@ class Calculate {
         case "x":
             return String(num1 * num2)
         case "÷":
+            if num2 == 0 { throw error.Error }
             return String(num1 / num2)
         default:
-            return ""
+            throw error.Error
         }
     }
 
-    func addInStack(_ elem: String) {
+    func addInStack(_ elem: String) throws {
         if isNumber(elem) && !stack.isEmpty && (stack.last == "x" || stack.last == "÷") {
             let oper = stack.removeLast()
             guard let num1 = Double(stack.removeLast()) else { return }
             guard let num2 = Double(elem) else { return }
-            stack.append(executeOper(oper: oper, num1: num1, num2: num2))
+            guard let appended = try? executeOper(oper: oper, num1: num1, num2: num2) else { throw error.Error}
+            stack.append(appended)
         }
         else {
             stack.append(elem)
         }
+    }
+    
+    func roundNumer(_ number: String) -> String {
+        if !number.contains(".") { return number }
+        var revNum = String(number.reversed())
+        var lenZero = 0
+
+        for elem in revNum {
+            if elem != "0" { break }
+            lenZero += 1
+        }
+        revNum.removeFirst(lenZero + 1)
+        return String(revNum.reversed())
     }
     
     func result() -> String {
@@ -57,10 +73,10 @@ class Calculate {
             guard let num1 = Double(stack.removeFirst()) else { return ""}
             let oper = stack.removeFirst()
             guard let num2 = Double(stack.removeFirst()) else { return ""}
-            stack.insert(executeOper(oper: oper, num1: num1, num2: num2), at: 0)
+            stack.insert(try! executeOper(oper: oper, num1: num1, num2: num2), at: 0)
             countElems -= 2
         }
-        return stack.removeFirst()
+        return roundNumer(stack.removeFirst())
     }
 }
 
@@ -68,13 +84,15 @@ class ViewController: UIViewController {
 
     var calc = Calculate()
     var ac = true
+    var errorStatus = false
+    var resultStatus = false
     
     @IBOutlet weak var elementsLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var clear: UIButton!
     
     func joinInLabel(_ appended: String, _ label: UILabel) {
-        if label.text == "0" && appended.count != 3 {
+        if label.text == "0" && calc.isNumber(appended) {
             label.text = appended
         }
         else if label == elementsLabel || label.text != "-0" {
@@ -84,31 +102,40 @@ class ViewController: UIViewController {
             label.text = "-" + appended
         }
     }
-
-    @IBAction func numbers(_ sender: UIButton) {
-        joinInLabel(String(sender.tag), resultLabel)
-        if sender.tag != 0 && ac == true {
-            ac = false
+    
+    func turnAc(to relay: Bool) {
+        ac = relay
+        if ac == true {
+            clear.setTitle("AC", for: .normal)
+        }
+        else {
             clear.setTitle("C", for: .normal)
         }
+    }
+
+    @IBAction func numbers(_ sender: UIButton) {
+        if errorStatus == true || resultStatus == true { return }
+        joinInLabel(String(sender.tag), resultLabel)
+        if sender.tag != 0 && ac == true {
+            turnAc(to: false)
+        }
         if !calc.lastOper.isEmpty {
-            calc.addInStack(calc.lastOper)
+            try! calc.addInStack(calc.lastOper)
             calc.lastOper = ""
         }
     }
 
     @IBAction func dot() {
+        if errorStatus == true || resultStatus == true { return }
         if resultLabel.text?.contains(".") == false {
             resultLabel.text?.append(".")
         }
         if ac == true {
-            ac = false
-            clear.setTitle("C", for: .normal)
+            turnAc(to: false)
         }
     }
     
-    @IBAction func opers(_ sender: UIButton) {
-        let oper = ["+", "-", "x", "÷"]
+    func checkNumberBeforAppOper() {
         guard var number = resultLabel.text else { return }
 
         if number.last == "." {
@@ -117,12 +144,25 @@ class ViewController: UIViewController {
         }
         if calc.lastOper.isEmpty {
             joinInLabel(number, elementsLabel)
-            calc.addInStack(number)
-            resultLabel.text = "0"
+            if ((try? calc.addInStack(number)) != nil) {
+                resultLabel.text = "0"
+            }
+            else {
+                resultLabel.text = "Error"
+                turnAc(to: true)
+                errorStatus = true
+            }
         }
         else {
             elementsLabel.text?.removeLast(3)
         }
+    }
+    
+    @IBAction func opers(_ sender: UIButton) {
+        if errorStatus == true { return }
+        if resultStatus == true { resultStatus = false }
+        let oper = ["+", "-", "x", "÷"]
+        checkNumberBeforAppOper()
         if !calc.stack.isEmpty {
             joinInLabel(" \(oper[sender.tag]) ", elementsLabel)
             calc.lastOper = oper[sender.tag]
@@ -130,6 +170,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func plusMinus() {
+        if errorStatus == true || resultStatus == true { return }
         guard let num = resultLabel.text else { return }
         if num.first == "-" {
             resultLabel.text?.removeFirst()
@@ -141,41 +182,32 @@ class ViewController: UIViewController {
     
     @IBAction func percent() {
     }
+
     @IBAction func result() {
+        if errorStatus == true { return }
         if !calc.stack.isEmpty {
-            guard var number = resultLabel.text else { return }
-            if number.last == "." {
-                number.removeLast()
-                resultLabel.text?.removeLast()
+            checkNumberBeforAppOper()
+            if errorStatus == false {
+                joinInLabel(" = ", elementsLabel)
+                resultLabel.text = calc.result()
+                calc.lastOper = ""
+                turnAc(to: true)
+                resultStatus = true
             }
-            if calc.lastOper.isEmpty {
-                joinInLabel(number, elementsLabel)
-                calc.addInStack(number)
-                resultLabel.text = "0"
-            }
-            else {
-                elementsLabel.text?.removeLast(3)
-            }
-            joinInLabel(" = ", elementsLabel)
-            resultLabel.text = calc.result()
-            calc.lastOper = ""
-            ac = true
-            clear.setTitle("AC", for: .normal)
         }
     }
-    
-    
+
     @IBAction func clearResult(_ sender: UIButton) {
         if ac == false {
             resultLabel.text = "0"
-            ac = true
-            clear.setTitle("AC", for: .normal)
+            turnAc(to: true)
         }
         else {
             elementsLabel.text = "0"
             resultLabel.text = "0"
             calc.stack.removeAll()
             calc.lastOper = ""
+            errorStatus = false
         }
     }
 }
